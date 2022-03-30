d93b298156ed1e9ef7a894d62e0f1a7f07f4e8af
badstieber <pascal.badstieber@gmx.de>
2022-03-30 15:06:57 +0200

    feat(step 8): Translatable Texts

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,14 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
-], (Controller, MessageToast, JSONModel) => {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], (Controller, MessageToast, JSONModel, ResourceModel) => {
     "use strict";
     return Controller.extend("badstieber.walkthrough.controller.App", {
-
+        /**
+         * onInit is one of OpenUI5’s lifecycle methods that is invoked by the framework when the controller is created, similar to a constructor function of a control
+         */
         onInit: function () {
             // set data model on view
             const oData = {
@@ -15,10 +18,23 @@ sap.ui.define([
             };
             const oModel = new JSONModel(oData);
             this.getView().setModel(oModel);
+            // set i18n model on view
+            const i18nModel = new ResourceModel({
+                bundleName: "badstieber.walkthrough.i18n.i18n"
+            });
+            this.getView().setModel(i18nModel, "i18n");
         },
 
-        onShowHello: () => {
-            MessageToast.show("Hey jo Diggi!! Was geht ab?!");
+        /**
+         * handles the button's press event by showing text
+         */
+        onShowHello: function () {
+            // read msg from i18n model
+            const oBundle = this.getView().getModel("i18n").getResourceBundle();
+            const sWombatTheDude = this.getView().getModel().getProperty("/wombatTheDude/name");
+            const sMsg = oBundle.getText("inputFieldDescriptionMsg", [sWombatTheDude]);
+            // show message
+            MessageToast.show(sMsg);
         }
 
     });
