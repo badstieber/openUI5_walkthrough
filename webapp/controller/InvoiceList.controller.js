bc51de176fb349a06b4b92d50a1e8a32e7f52478
badstieber <pascal.badstieber@gmx.de>
2022-04-04 12:22:56 +0200

    feat(step 23): Filtering
    In this step, we add a search field for our product list and define a filter that represents the search term. When searching, the list is automatically updated to show only the items that match the search term.

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,19 +1,35 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "../model/formatter"
-], function (Controller, JSONModel, formatter) {
+    "../model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+], function (Controller, JSONModel, formatter, Filter, FilterOperator) {
     "use strict";
 
     return Controller.extend("badstieber.walkthrough.controller.InvoiceList", {
 
         formatter: formatter,
-        
+
         onInit: function () {
             var oViewModel = new JSONModel({
                 currency: "EUR in Bar Diggi"
             });
             this.getView().setModel(oViewModel, "view");
+        },
+
+        onFilterInvoices: function (oEvent) {
+            // build fiter array
+            let aFilter = [];
+            let sQuery = oEvent.getParameter("query");
+            if (sQuery) {
+                aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+            }
+
+            // filter binding
+            let oList = this.byId("invoiceList");
+            let oBinding = oList.getBinding("items");
+            oBinding.filter(aFilter);
         }
 
     });
