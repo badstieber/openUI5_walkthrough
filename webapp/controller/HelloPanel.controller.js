344f531c80c3b91dff6b683bce770aa819dec642
badstieber <pascal.badstieber@gmx.de>
2022-04-04 09:46:40 +0200

    feat(step 16): Dialogs and Fragments
    In this step, we will take a closer look at another element which can be used to assemble views: the fragment.

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,9 +1,10 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-], function (Controller, MessageToast) {
+    "sap/ui/core/Fragment"
+], function (Controller, MessageToast, Fragment) {
     "use strict";
-    return Controller.extend("badstieber.walkthrough.controller.App", {
+    return Controller.extend("badstieber.walkthrough.controller.HelloPanel", {
 
         /**
          * handles the button's press event by showing text
@@ -15,7 +16,23 @@ sap.ui.define([
             const sMsg = oBundle.getText("inputFieldDescriptionMsg", [sWombatTheDude]);
             // show message
             MessageToast.show(sMsg);
+        },
+
+        /**
+         * handles fragment
+         */
+        onOpenDialog: function () {
+            // create dialog lazily
+            if (!this.pDialog) {
+                this.pDialog = this.loadFragment({
+                    name: "badstieber.walkthrough.view.HelloDialog"
+                });
+            }
+            this.pDialog.then(function(oDialog) {
+                oDialog.open();
+            });
         }
+
 
     });
 
